cba37933fc78b0e77c59a2a74b7f8abc40f84901
DESKTOP-NDCBFHM\MrUser <robert.kamenski.rld@gmail.com>
2025-03-06 22:09:28 +0100

    feat: Add Tests with a test written for Clean Architecture, Move ScaffNet to it's own folder, run dotnet test inside CI

---
+++ Tests/ArchitectureTests/CleanArchitectureTest.cs
@@ -0,0 +1,29 @@
+﻿using ScaffNet.Features.CleanArchitecture;
+
+namespace Tests.ArchitectureTests
+{
+    public class CleanArchitectureTests
+    {
+        [Fact]
+        public void Create_DefaultArgs_ShouldGenerateBuildAndDeleteSolution()
+        {
+            var solutionName = "TestCleanArchitectureSolution";
+            var args = new CleanArchitectureArgs()
+            {
+                SolutionName = solutionName,
+                SolutionPath = solutionName
+            };
+            
+            var exception = Record.Exception(() => CleanArchitectureScaffolder.Create(args));
+
+            Assert.Null(exception);
+
+            var testSolutionFolder = Path.Combine(AppContext.BaseDirectory, solutionName);
+            string expectedSolutionPath = Path.Combine(testSolutionFolder, $"{solutionName}.sln");
+            Assert.True(File.Exists(expectedSolutionPath), "Solution file should have been created.");
+
+            Directory.Delete(testSolutionFolder, true);
+            Assert.False(Directory.Exists(testSolutionFolder));
+        }
+    }
+}
